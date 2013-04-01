4d5fbb8132d865e3cc26ed08fbd841f2ebf7a895
Lee Watson <synystershadows403@googlemail.com>
2013-04-01 16:16:44 +0100

    Initial commit

---
+++ src/main/java/com/notoriousdev/bungeetools/BungeeTools.java
@@ -0,0 +1,12 @@
+package com.notoriousdev.bungeetools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: The Reverend
+ * Date: 01/04/13
+ * Time: 16:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class BungeeTools
+{
+}
